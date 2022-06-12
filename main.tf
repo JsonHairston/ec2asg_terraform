d1917ac0f6d0e09a7f9a157d99c09b1e443e5839
Ubuntu <j.hairston56@gmail.com>
2022-06-12 19:57:49 +0000

    corrected code

--- main.tf
+++ main.tf
@@ -27,26 +27,6 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
-#Create internet gateway
-resource "aws_internet_gateway" "testigw" {
-  vpc_id = aws_vpc.testvpc.id
-
-  tags = {
-    Name = "TestIGW"
-  }
-}
-
-#Routing table
-resource "aws_route_table" "newtestrt" {
-  vpc_id = aws_vpc.testvpc.id
-    
- tags = {
-    Name = "NewTestRT"
-     
- }
-    
-} 
-
 #Security groups
 resource "aws_security_group" "allow_all" {
   name        = "allow_all"
@@ -58,7 +38,7 @@ resource "aws_security_group" "allow_all" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.testvpc.cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
@@ -74,32 +54,37 @@ resource "aws_security_group" "allow_all" {
   }
 }
 
-#Launch Template for EC2
-resource "aws_launch_template" "testserver" {
-  name = "testserver"
+#Create internet gateway
+resource "aws_internet_gateway" "testigw" {
+  vpc_id = aws_vpc.testvpc.id
 
-  image_id = "ami-0022f774911c1d690"
-
-  instance_type = "t2.micro"
-
-  monitoring {
-    enabled = true
-  }
-
-  placement {
-    availability_zone = "us-east-1"
-  }
-
-  vpc_security_group_ids = [aws_security_group.allow_all.id]
-
-  tag_specifications {
-    resource_type = "instance"
-
-    tags = {
-      Name = "testserver"
-    }
+  tags = {
+    Name = "TestIGW"
   }
 }
 
+#Routing table
+resource "aws_route_table" "newtestrt" {
+  vpc_id = aws_vpc.testvpc.id
+    
+ tags = {
+    Name = "NewTestRT"
+ }
+} 
+
 #   user_data = filebase64("${path.module}/apache.sh")
 # }
+resource "aws_instance" "testserver" {
+  ami = "ami-0022f774911c1d690"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+  subnet_id = aws_subnet.public_subnet.id
+  associate_public_ip_address = true
+  
+  monitoring = true
+  
+  tags = {
+    name = "testServer"
+  }
+  
+}
