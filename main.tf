04f4a2d6a54a4f4202d47bbd3872a8c638785441
Ubuntu <j.hairston56@gmail.com>
2022-06-12 04:57:32 +0000

    added ec2 launch template

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-erraform {
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -20,7 +20,7 @@ resource "aws_vpc" "testvpc" {
 #Create subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.testvpc.id
-  cidr_block = "10.10.1.0/16"
+  cidr_block = "10.10.0.0/16"
 
   tags = {
     Name = "PublicSubnet"
@@ -36,31 +36,16 @@ resource "aws_internet_gateway" "testigw" {
   }
 }
 
-#Attach IGW to VPC
-resource "aws_internet_gateway_attachment" "igwattachment" {
-  internet_gateway_id = aws_internet_gateway.testigw.id
-  vpc_id              = aws_vpc.testvpc.id
-}
-
-resource "aws_vpc" "testvpc" {
-  cidr_block = "10.10.0.0/16"
-}
-
-resource "aws_internet_gateway" "testigw" {}
-
 #Routing table
 resource "aws_route_table" "newtestrt" {
   vpc_id = aws_vpc.testvpc.id
-
-  route = {
-    cidr_block = "10.10.1.0/16"
-    gateway_id = aws_internet_gateway.testigw.id
-  }
-
+    
  tags = {
     Name = "NewTestRT"
-  }
-}
+     
+ }
+    
+} 
 
 #Security groups
 resource "aws_security_group" "allow_all" {
@@ -87,4 +72,97 @@ resource "aws_security_group" "allow_all" {
   tags = {
     Name = "allow_all"
   }
+}
+
+#Launch Template for EC2
+resource "aws_launch_template" "Testec2" {
+  name = "foo"
+
+  block_device_mappings {
+    device_name = "/dev/sda1"
+
+    ebs {
+      volume_size = 20
+    }
+  }
+
+  capacity_reservation_specification {
+    capacity_reservation_preference = "open"
+  }
+
+  cpu_options {
+    core_count       = 4
+    threads_per_core = 2
+  }
+
+  credit_specification {
+    cpu_credits = "standard"
+  }
+
+  disable_api_termination = true
+
+  ebs_optimized = true
+
+  elastic_gpu_specifications {
+    type = "test"
+  }
+
+  elastic_inference_accelerator {
+    type = "eia1.medium"
+  }
+
+  iam_instance_profile {
+    name = "test"
+  }
+
+  image_id = "ami-test"
+
+  instance_initiated_shutdown_behavior = "terminate"
+
+  instance_market_options {
+    market_type = "spot"
+  }
+
+  instance_type = "t2.micro"
+
+  kernel_id = "test"
+
+  key_name = "test"
+
+  license_specification {
+    license_configuration_arn = "arn:aws:license-manager:eu-west-1:123456789012:license-configuration:lic-0123456789abcdef0123456789abcdef"
+  }
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+    instance_metadata_tags      = "enabled"
+  }
+
+  monitoring {
+    enabled = true
+  }
+
+  network_interfaces {
+    associate_public_ip_address = true
+  }
+
+  placement {
+    availability_zone = "us-west-2a"
+  }
+
+  ram_disk_id = "test"
+
+  vpc_security_group_ids = ["sg-12345678"]
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "test"
+    }
+  }
+
+  user_data = filebase64("${path.module}/example.sh")
 }
