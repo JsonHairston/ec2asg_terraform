e9e2ab2f85590b490ea33614ea3f591b9bfac8b6
Ubuntu <j.hairston56@gmail.com>
2022-06-12 22:21:44 +0000

    fine tuning main.tf

--- main.tf
+++ main.tf
@@ -12,12 +12,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Create a VPC
+## Create a VPC
 resource "aws_vpc" "testvpc" {
   cidr_block = "10.10.0.0/16"
 }
 
-#Create subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.testvpc.id
   cidr_block = "10.10.0.0/16"
@@ -27,34 +26,6 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
-#Security groups
-resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
-  description = "Allow inbound traffic"
-  vpc_id      = aws_vpc.testvpc.id
-
-  ingress {
-    description = "Allow traffic"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_all"
-  }
-}
-
-#Create internet gateway
 resource "aws_internet_gateway" "testigw" {
   vpc_id = aws_vpc.testvpc.id
 
@@ -63,17 +34,25 @@ resource "aws_internet_gateway" "testigw" {
   }
 }
 
-#Routing table
 resource "aws_route_table" "newtestrt" {
   vpc_id = aws_vpc.testvpc.id
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.testigw.id
+    }
 
   tags = {
     Name = "NewTestRT"
   }
 }
 
-#   user_data = filebase64("${path.module}/apache.sh")
-# }
+resource "aws_route_table_association" "public_rt_asso" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.newtestrt.id
+}
+
+## Creating Instance Resources
 resource "aws_instance" "testserver" {
   ami                         = "ami-0022f774911c1d690"
   instance_type               = "t2.micro"
@@ -88,14 +67,13 @@ resource "aws_instance" "testserver" {
   tags = {
     name = "testServer"
   }
-
 }
 
-#Create ASG 
 resource "aws_launch_configuration" "terraform_config" {
   name          = "web_config"
   image_id      = "ami-0022f774911c1d690"
   instance_type = "t2.micro"
+   user_data = file("apache.sh")
 
   lifecycle {
     create_before_destroy = true
@@ -115,4 +93,56 @@ resource "aws_autoscaling_group" "terraform_asg" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+## Security groups
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.testvpc.id
+  
+
+  ingress {
+    description = "Allow traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  
+  ingress {
+    description = "Allow traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
+
+#Outputs
+output "web_instance_ip" {
+    value = aws_instance.testserver.public_ip
 }
